48a3361f8d25f11bd619d835f718d0aca5aab063
Adrielanders <Adrielanderson.s@gmail.com>
2024-09-03 09:55:56 +0700

    new file:   WR_FrontEnd/Public/index.php 	deleted:    WR_FrontEnd/Public/login.php 	new file:   WR_FrontEnd/_Global/CSS/login.css 	new file:   WR_FrontEnd/resources/views/layout/app.php

---
+++ WR_FrontEnd/Public/index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Resto-Login</title>
+    
+</head>
+
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="#">
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="text" id="password" name="password" placeholder="Password" required>
+            </div>
+            <button type="submit" class="btn-login">Login</button>
+            <a href="#" class="forgot-password">Forgot Password?</a>
+        </form>
+    </div>
+</body>
+
+</html>
