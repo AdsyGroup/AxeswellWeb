68d78f676c4068580169e5954b7c6eaa0509c9db
Adrielanders <Adrielanderson.s@gmail.com>
2024-10-23 14:35:16 +0700

    modified:   Axeswell_FrontEnd/_Global/JS/index.js

--- Axeswell_FrontEnd/_Global/JS/index.js
+++ Axeswell_FrontEnd/_Global/JS/index.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
     $('.ui.checkbox').checkbox();
 
     $('#btn-login').on('click',function(){
-     
     var username = $('#username').val();
     var password = $('#password').val();
-      
+    
+    console.log(username);
     });
+
+
 
 
    
